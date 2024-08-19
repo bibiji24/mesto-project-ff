34845d2f715d0263cc1499a7b76c058d4de4a13e
Leonid Nikiforov <bibiji@yandex.ru>
2024-08-19 13:17:45 +0900

    Project 5: initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(name, link, deleteCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const avatar = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const deleteButton = card.querySelector('.card__delete-button');
+    avatar.setAttribute('src', link);
+    avatar.setAttribute('alt', name);
+    cardTitle.textContent = name;
+    deleteButton.addEventListener('click', deleteCard);
+    return card;
+}
 // @todo: Функция удаления карточки
+function removeCard(evt) {
+    const card = evt.target.closest('.card');
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => {
+    placesList.append(createCard(item.name, item.link, removeCard));
+});
